ae8ca02ef828be06f889d8a992c90e5920435d3c
gabriel <147539116+gargoyle-alarm@users.noreply.github.com>
2024-02-09 12:17:45 -0600

    in-class update

--- GBinarySearchTree/src/panel.java
+++ GBinarySearchTree/src/panel.java
@@ -14,11 +14,40 @@ public class panel extends JPanel implements MouseListener {
 
 	
 	private int alpha = 0;
-	Color bScreen = new Color(0, 0, 0, 125);
+	//Color template = new Color(0, 0, 0, alpha);
+	Color color;
+	private BufferedImage ref; // this sucks; calc layout >>>>>
+	
 	
 	public panel() {
-			
-	} // end of paint
+		BinarySearchTree tree = new BinarySearchTree();
+		try {
+			ref = ImageIO.read(panel.class.getResource("/images/dook.png"));
+		}
+		catch (Exception e) {
+			out.println("Exception Error");
+			return;
+		}
+		addMouseListener(this);	
+	} // end of panel
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		//bg
+			color = new Color(126, 143, 126);
+			g.setColor(color);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
+			//g.drawImage(ref, 0, 0, getWidth(), getHeight(), null); 
+			g.setColor(color.white);
+			int dim = 75;
+			for(int i = 0; i < 5; i++) {
+				//2, 3, 5, 9, 17
+				//2^i + 1
+			}
+			g.drawOval(getWidth()/2 - (dim/2), 30, dim, dim);
+			g.drawOval(getWidth()/3 - (dim/2), 30 + (int) (dim * 0.75), dim, dim);
+	}
 	
 	public void mouseClicked(MouseEvent e) {
 	
